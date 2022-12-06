486ce9e85cf9edfcace0945bb9190aeac30f5e93
15244 <mohammad.amaan@teamcomputers.com>
2022-12-06 11:48:00 +0530

    signature

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:digitalsignatures/pages/homepage.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Digital Signature',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
